c4b4783879112dbc0f5677f5d76d24ac165053e1
Joey T <thechickenbane@gmail.com>
2018-04-23 20:09:51 -0700

    Update Kotlin, Junit 5

--- server/src/test/kotlin/github/joeyslalom/bootvault/BootVaultApplicationTests.kt
+++ server/src/test/kotlin/github/joeyslalom/bootvault/BootVaultApplicationTests.kt
@@ -1,16 +1,17 @@
 package github.joeyslalom.bootvault
 
 import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@RunWith(SpringRunner::class)
-@SpringBootTest
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class BootVaultApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
 }
